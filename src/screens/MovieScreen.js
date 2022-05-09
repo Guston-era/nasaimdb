0c1a16d5e34b6f2062a552d41ac51dba0d4d0871
Guston-era <me@gustonera.co.ke>
2022-05-09 14:32:51 +0300

    added style to movie screen

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import './css/MovieScreen.css'
 
 const MovieScreen = () => {
   const { movieId } = useParams()

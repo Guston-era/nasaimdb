7b23c3235c95dc122d0f79d54bc6bbb5bf428fe4
Guston-era <me@gustonera.co.ke>
2022-05-09 14:39:11 +0300

    added not found file

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <div align="center">
+      <h2>Error 404:</h2>
+      The file You requested was not found
+    </div>
+  )
 }
 
 export default NotFound
